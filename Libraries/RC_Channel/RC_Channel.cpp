9b9f32d323500816b422f23dcef44331d0a88f22
SuWeipeng <weipeng.su@outlook.com>
2019-08-28 10:26:45 +0800

    加入 ssd1306 oled 驱动并调试 ok

--- Libraries/RC_Channel/RC_Channel.cpp
+++ Libraries/RC_Channel/RC_Channel.cpp
@@ -1,6 +1,7 @@
 #include <AP_Math.h>
 #include "usb_device.h"
 #include "RC_Channel.h"
+#include "ssd1306.h"
 
 RC_Channel::RC_Channel(ADC_HandleTypeDef* hadc)
   : _hadc(hadc)
@@ -22,28 +23,31 @@ uint32_t RC_Channel::get_value(uint8_t channel)
 float RC_Channel::vel_x(int8_t inv)
 {
   float    ret = 0.0f;
-  uint16_t min = 0;
+  uint16_t min = ADC_CHANNEL_X_MIN;
   uint16_t mid = ADC_CHANNEL_X_MID;
   uint16_t max = ADC_CHANNEL_X_MAX;
   uint16_t val = get_value(3);
   uint16_t mid_zone_up   = mid + ADC_DEAD_ZONE;
   uint16_t mid_zone_down = mid - ADC_DEAD_ZONE;
 
-  if(abs(val-mid) < ADC_DEAD_ZONE) return 0.0f;
-
-  if(val > mid) {
+  if(abs(val-mid) < ADC_DEAD_ZONE){
+    ret = 0.0f;
+  } else if(val > mid) {
     ret = (val-mid_zone_up)*(VEL_X_MAX_M_S/(max-mid_zone_up));
   } else {
     ret = (val-mid_zone_down)*(VEL_X_MAX_M_S/(mid_zone_down-min));
   }
   
   if(inv == -1) ret *= inv;
-  
-#if ADC_VCP_DEBUG == 1  
+
   char buffer[30];
-  sprintf(buffer, "vel_x:%.3f\r\n", ret);
+  sprintf(buffer, "vel_x:%.3f m/s\r\n", ret);  
+#if ADC_VCP_DEBUG == 1  
   VCPSend((uint8_t *)buffer, strlen(buffer));
 #endif 
+  ssd1306_SetCursor(2, 18*0);
+  ssd1306_WriteString(buffer, Font_7x10, White);
+  ssd1306_UpdateScreen();
   
   return constrain_float(ret, -VEL_X_MAX_M_S, VEL_X_MAX_M_S);
 }
@@ -51,28 +55,31 @@ float RC_Channel::vel_x(int8_t inv)
 float RC_Channel::vel_y(int8_t inv)
 {
   float    ret = 0.0f;
-  uint16_t min = 0;
+  uint16_t min = ADC_CHANNEL_Y_MIN;
   uint16_t mid = ADC_CHANNEL_Y_MID;
   uint16_t max = ADC_CHANNEL_Y_MAX;
   uint16_t val = get_value(0);
   uint16_t mid_zone_up   = mid + ADC_DEAD_ZONE;
   uint16_t mid_zone_down = mid - ADC_DEAD_ZONE;
 
-  if(abs(val-mid) < ADC_DEAD_ZONE) return 0.0f;
-
-  if(val > mid) {
+  if(abs(val-mid) < ADC_DEAD_ZONE){
+    ret = 0.0f;
+  } else if(val > mid) {
     ret = (val-mid_zone_up)*(VEL_Y_MAX_M_S/(max-mid_zone_up));
   } else {
     ret = (val-mid_zone_down)*(VEL_Y_MAX_M_S/(mid_zone_down-min));
   }
   
   if(inv == -1) ret *= inv;
-  
-#if ADC_VCP_DEBUG == 1  
+
   char buffer[30];
-  sprintf(buffer, "vel_y:%.3f\r\n", ret);
+  sprintf(buffer, "vel_y:%.3f m/s\r\n", ret);  
+#if ADC_VCP_DEBUG == 1  
   VCPSend((uint8_t *)buffer, strlen(buffer));
 #endif  
+  ssd1306_SetCursor(2, 18*1);
+  ssd1306_WriteString(buffer, Font_7x10, White);
+  ssd1306_UpdateScreen();  
   
   return constrain_float(ret, -VEL_Y_MAX_M_S, VEL_Y_MAX_M_S);
 }
@@ -80,28 +87,31 @@ float RC_Channel::vel_y(int8_t inv)
 float RC_Channel::rad_z(int8_t inv)
 {
   float    ret = 0.0f;
-  uint16_t min = 0;
+  uint16_t min = ADC_CHANNEL_Z_MIN;
   uint16_t mid = ADC_CHANNEL_Z_MID;
   uint16_t max = ADC_CHANNEL_Z_MAX;
   uint16_t val = get_value(2);
   uint16_t mid_zone_up   = mid + ADC_DEAD_ZONE;
   uint16_t mid_zone_down = mid - ADC_DEAD_ZONE;
 
-  if(abs(val-mid) < ADC_DEAD_ZONE) return 0.0f;
-
-  if(val > mid) {
+  if(abs(val-mid) < ADC_DEAD_ZONE){
+    ret = 0.0f;
+  } else if(val > mid) {
     ret = (val-mid_zone_up)*(RAD_Z_MAX_RAD_S/(max-mid_zone_up));
   } else {
     ret = (val-mid_zone_down)*(RAD_Z_MAX_RAD_S/(mid_zone_down-min));
   }
   
   if(inv == -1) ret *= inv;
-  
-#if ADC_VCP_DEBUG == 1  
+
   char buffer[30];
-  sprintf(buffer, "rad_z:%.3f\r\n", ret);
+  sprintf(buffer, "rad_z:%.3frad/s\r\n", ret);  
+#if ADC_VCP_DEBUG == 1  
   VCPSend((uint8_t *)buffer, strlen(buffer));
 #endif 
+  ssd1306_SetCursor(2, 18*2);
+  ssd1306_WriteString(buffer, Font_7x10, White);
+  ssd1306_UpdateScreen(); 
   
   return constrain_float(ret, -RAD_Z_MAX_RAD_S, RAD_Z_MAX_RAD_S);
 }
--- Src/main.c
+++ Src/main.c
@@ -26,6 +26,7 @@
 /* USER CODE BEGIN Includes */
 #include "MY_NRF24.h"
 #include "nrf_mavlink.h"
+#include "ssd1306.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -48,6 +49,7 @@ ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
 SPI_HandleTypeDef hspi1;
+SPI_HandleTypeDef hspi2;
 
 UART_HandleTypeDef huart1;
 
@@ -62,6 +64,7 @@ static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_SPI2_Init(void);
 /* USER CODE BEGIN PFP */
 void setup(void);
 void loop(void);
@@ -105,6 +108,7 @@ int main(void)
   MX_ADC1_Init();
   MX_SPI1_Init();
   MX_USART1_UART_Init();
+  MX_SPI2_Init();
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
   NRF24_begin(NRF_CE_GPIO_Port, NRF_CS_Pin, NRF_CE_Pin, hspi1);
@@ -119,6 +123,8 @@ int main(void)
   NRF24_enableAckPayload();
 	
   NRF24_startListening();
+  
+  ssd1306_Init();
   
   setup();
   /* USER CODE END 2 */
@@ -289,6 +295,44 @@ static void MX_SPI1_Init(void)
 }
 
 /**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_1LINE;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
   * @brief USART1 Initialization Function
   * @param None
   * @retval None
@@ -353,12 +397,22 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, NRF_CE_Pin|NRF_CS_Pin, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12|GPIO_PIN_14, GPIO_PIN_RESET);
+
   /*Configure GPIO pins : NRF_CE_Pin NRF_CS_Pin */
   GPIO_InitStruct.Pin = NRF_CE_Pin|NRF_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB12 PB14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
