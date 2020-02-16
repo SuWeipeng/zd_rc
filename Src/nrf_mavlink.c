f2742225dfb4dd116ad56ac6c9fa2b619c5b60e6
SuWeipeng <weipeng.su@outlook.com>
2020-02-16 16:43:00 +0800

    用遥控器上的 KEY_R 终止 pwm_rpm 实验（按下链则 Matlab 上位机停止采样绘图）

--- Src/nrf_mavlink.c
+++ Src/nrf_mavlink.c
@@ -17,26 +17,31 @@ void update_mavlink(void)
     for(i=0; i<32; i++) {
       if(mavlink_parse_char(0, myRxData[i], &msg_receive, &mav_status)) {
         switch (msg_receive.msgid) {
-
         case MAVLINK_MSG_ID_SIMPLE: {
           mavlink_simple_t packet;
           mavlink_msg_simple_decode(&msg_receive, &packet);
 
-					/*
+          /*
           char buffer[32];
           sprintf(buffer, "%d\r\n", packet.data);
           VCPSend((uint8_t *)buffer, strlen(buffer));
-					*/
+          */
 
           mavlink_message_t msg_ack;
           mavlink_msg_velocity_pack(0, 0, &msg_ack, vel.vel_x, vel.vel_y, vel.rad_z);
           int len = mavlink_msg_to_send_buffer((uint8_t *)myAckPayload, &msg_ack);
           NRF24_writeAckPayload(1, myAckPayload, len);
-					VCPSend((uint8_t *)myAckPayload, len);
+          VCPSend((uint8_t *)myAckPayload, len);
           break;
         }
         }
       }
     }
+  }
+  if(HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_15) == GPIO_PIN_RESET){
+    mavlink_message_t msg;
+    mavlink_msg_cmd_pack( 0, 0, &msg, 1 );
+    int len = mavlink_msg_to_send_buffer((uint8_t *)myAckPayload, &msg);
+    NRF24_writeAckPayload(1, myAckPayload, len);
   }
 }
