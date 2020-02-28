d4c17f93eaf3624ae7ddb6faa5f13eed2119bb9d
SuWeipeng <weipeng.su@outlook.com>
2020-02-28 21:32:31 +0800

    加入 AP_Buffer 并测试通过

---
+++ Inc/rtt_interface.h
@@ -0,0 +1,14 @@
+#ifndef __RTT_INTERFACE_H
+#define __RTT_INTERFACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define AP_BUFFER_MAX_SIZE  (6)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RTT_INTERFACE_H */
--- Src/user_main.cpp
+++ Src/user_main.cpp
@@ -1,12 +1,14 @@
 #include "nrf_mavlink.h"
 #include "RC_Channel.h"
 #include "AP_Show.h"
+#include "AP_Buffer.h"
 
 extern ADC_HandleTypeDef hadc1;
 extern vel_target vel;
 
 RC_Channel* rc;
 AP_Show* show;
+AP_Buffer *buffer;
 
 #if defined(__GNUC__) && defined(__cplusplus)
 extern "C" {
@@ -17,6 +19,8 @@ void setup(void)
   rc = new RC_Channel(&hadc1);
   show = new AP_Show();
   show->init(AP_Show::SSD1306_OLED_SPI);
+  buffer = new AP_Buffer();
+  buffer->init(AP_Buffer::RING);
 }
 
 void loop(void)
@@ -43,8 +47,24 @@ void loop(void)
     vel.rad_z = rc->vel_y_or_z(-1, 1);
   }
   
-  if(HAL_GetTick()-time_stamp >= 100){
-    show->show_page(0);
+  if(HAL_GetTick()-time_stamp >= 500){
+    show->show_page(1);
+    
+    // Page 1
+    static uint32_t cnt = 0;
+    char buf[DISP_MAX_CHAR_PER_LINE];
+    char c[1];
+    sprintf(c, "%d", cnt++ % 10);
+    buffer->write(c,sizeof(c)); 
+    sprintf(buf, "buf :%s \r\n", (uint8_t*)buffer->get_buffer());
+    show->page_write(1, 0, buf, "ring fifo");
+    sprintf(buf, "len :%d \r\n", buffer->buf_len());
+    show->page_write(1, 1, buf, "ring fifo");
+    if(cnt%3==0 && buffer->read()>0){
+      sprintf(buf, "read:%s \r\n", (uint8_t*)buffer->read_buf_addr());
+      show->page_write(1, 2, buf, "ring fifo");
+    }
+    
     show->update();
     time_stamp = HAL_GetTick();
   }
