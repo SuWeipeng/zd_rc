d65b11a06d78b2287386044976d8bef0c8eec31a
SuWeipeng <weipeng.su@outlook.com>
2020-03-02 15:20:55 +0800

    在应用层测试 FIFO

--- Src/user_main.cpp
+++ Src/user_main.cpp
@@ -20,7 +20,7 @@ void setup(void)
   show = new AP_Show();
   show->init(AP_Show::SSD1306_OLED_SPI);
   buffer = new AP_Buffer();
-  buffer->init(AP_Buffer::RING);
+  buffer->init(AP_Buffer::FIFO);
 }
 
 void loop(void)
@@ -55,12 +55,13 @@ void loop(void)
     char buf[DISP_MAX_CHAR_PER_LINE];
     char c[1];
     sprintf(c, "%d", cnt++ % 10);
-    buffer->write(c,sizeof(c)); 
+//    buffer->write(c,sizeof(c)); 
+    buffer->write("buffer",6); 
     sprintf(buf, "buf :%s \r\n", (uint8_t*)buffer->get_buffer());
     show->page_write(1, 0, buf, "ring fifo");
     sprintf(buf, "len :%d \r\n", buffer->buf_len());
     show->page_write(1, 1, buf, "ring fifo");
-    if(cnt%3==0 && buffer->read()>0){
+    if(cnt%1==0 && buffer->read()>0){
       sprintf(buf, "read:%s \r\n", (uint8_t*)buffer->read_buf_addr());
       show->page_write(1, 2, buf, "ring fifo");
     }
