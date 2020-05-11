7f41db5eea457062a6ea04018765b18d615b8734
ubuntu <ubuntu@ubuntu.com>
2020-05-11 17:43:09 +0800

    将 rt-thread 编译至无错

--- Inc/rtt_interface.h
+++ Inc/rtt_interface.h
@@ -6,7 +6,30 @@ extern "C" {
 #endif
 
 #define AP_BUFFER_MAX_SIZE  (6)
+#if defined(USE_RTTHREAD)
+#include <entry.h>
 
+#define LED_R_PIN           GET_PIN(B, 7)
+#define LED_B_PIN           GET_PIN(B, 3)
+#define K1                  GET_PIN(A, 8)
+#define K2                  GET_PIN(B, 10)
+#define K3                  GET_PIN(B, 11)
+#define K4                  GET_PIN(C, 15)
+
+typedef struct vel_target {
+  float vel_x; // m/s
+  float vel_y; // m/s
+  float rad_z; // rad/s
+} vel_target;
+
+extern rt_thread_t led_thread;
+extern rt_thread_t show_thread;
+
+void led_thread_entry(void* parameter);
+void show_thread_entry(void* parameter);
+
+int loop_start(void);
+#endif // #if defined(USE_RTTHREAD)
 #ifdef __cplusplus
 }
 #endif
---
+++ Src-rtt/thread_show.cpp
@@ -0,0 +1,71 @@
+#include "rtt_interface.h"
+#include <string.h>
+#include <entry.h>
+#include "AP_Show.h"
+#include "AP_Buffer.h"
+
+extern vel_target vel;
+extern AP_Show* show;
+extern AP_Buffer *buffer;
+extern uint8_t key_value;
+
+extern "C"{
+char global_buf[4][16];
+}
+
+rt_thread_t show_thread = RT_NULL;
+
+extern "C"
+void show_thread_entry(void* parameter)
+{  
+  char line[3][15];
+  char head[16];
+  int8_t  page_num = 0;
+  while(1) {
+    // Switch page
+    if(key_value == 2 && page_num != 1){
+      page_num--;
+      if(page_num < 0) page_num = 0;
+      show->show_now(page_num);
+      key_value = 0;
+    } else if(key_value == 12){
+      page_num--;
+      if(page_num < 0) page_num = 0;
+      show->show_now(page_num);
+      key_value = 0;
+    }
+
+    if(key_value == 1){
+      page_num++;
+      if(page_num > 1) page_num = 1;
+      show->show_now(page_num);
+      key_value = 0;
+    }
+  
+    // Show page
+    show->show_page(page_num);
+    
+    // Page 0
+    sprintf (line[0], "vel_x:%.3f", vel.vel_x);
+    sprintf (line[1], "vel_y:%.3f", vel.vel_y);
+    sprintf (line[2], "rad_z:%.3f", vel.rad_z);
+    for(uint8_t i=0; i<3; i++){
+      show->page_write(0, i, line[i], "rc output");
+    }
+    
+    // Page 1
+    sprintf(head, "%s \r\n", "key monitor");
+    show->page_write(1, 0, global_buf[0], head);
+    show->page_write(1, 2, global_buf[2], head);
+    if(key_value == 15){
+	memset(global_buf[2], 0, 16);
+    }
+    
+    // update screen
+    rt_enter_critical();
+    show->update();
+    rt_exit_critical();
+    
+    rt_thread_delay(500);
+  }
+}
